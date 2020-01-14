e9b0ed39424ba8307bb38674e5e5e5c46190c10c
123 <123@qq.com>
2020-01-14 12:44:10 +0800

    添加了user模块

--- tantan/settings.py
+++ tantan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'tantan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mys',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE":"django.db.backends.mysql",
+        "USER":"root",
+        "PASSWORD":"tzb520",
+        "HOST":"127.0.0.1",
+        "PORT":3306,
+        "NAME":"tantan",
+
     }
 }
 
